8a1ca4e8e65ba976b990c37ad2a237ea1260f2d0
Akash Savalagi <asavalagi@horizontal.com>
2025-06-06 18:27:25 +0530

    refactor: streamline SFTP upload process and improve code clarity in route.js

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -2,46 +2,35 @@ export const runtime = 'nodejs';
 
 import { NextResponse } from 'next/server';
 import axios from 'axios';
-import SFTPClient from 'ssh2-sftp-client';
 
 export async function GET() {
-    console.log('Cron job triggered at:', new Date().toISOString());
-    // URL of the sitemap.xml file
-  const sitemapUrl = 'https://ziploc.com/sitemap.xml'; // Replace with your actual URL
+  console.log('Cron job triggered at:', new Date().toISOString());
 
-  // SFTP configuration
+  const sitemapUrl = 'https://ziploc.com/sitemap.xml';
+
   const sftpConfig = {
     host: process.env.SFTP_HOST,
-    port:  22,  // Default SFTP port is 22
+    port: 22,
     username: process.env.SFTP_USERNAME,
     password: process.env.SFTP_PASSWORD,
-    // Optional: use privateKey and passphrase for authentication if needed
-    // privateKey: require('fs').readFileSync('/path/to/private/key'),
-    // passphrase: process.env.SFTP_PASSPHRASE,
   };
 
   try {
-    console.log('Cron job try block: ', new Date().toISOString());
-    // Step 1: Fetch the sitemap.xml file from the URL
     const response = await axios.get(sitemapUrl);
     const sitemapXml = response.data;
 
-    // Step 2: Connect to the SFTP server
+    // Dynamically import SFTP client
+    const SFTPClient = (await import('ssh2-sftp-client')).default;
     const sftp = new SFTPClient();
+
     await sftp.connect(sftpConfig);
-
-    // Step 3: Upload the sitemap.xml to the SFTP server
-    const remotePath = '/xml/sitemap1.xml'; // Specify the remote path on the SFTP server
-    await sftp.put(Buffer.from(sitemapXml), remotePath);
-
-    // Close the SFTP connection
+    await sftp.put(Buffer.from(sitemapXml), '/xml/sitemap1.xml');
     await sftp.end();
-    console.log('Cron job sftp end at:', new Date().toISOString());
-    // Return a success response
-    //res.status(200).json({ message: 'Sitemap uploaded successfully' });
+
+    console.log('Sitemap uploaded successfully');
   } catch (error) {
-    console.error('Error uploading sitemap:', error);
-    //res.status(500).json({ message: 'Failed to upload sitemap', error: error.message });
+    console.error('Sitemap upload error:', error);
   }
+
   return NextResponse.json({ ok: true });
 }
