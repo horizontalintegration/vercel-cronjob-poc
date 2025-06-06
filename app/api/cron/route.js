99d88244a1e7d4de501b075e35aa742ba80222cc
Akash Savalagi <asavalagi@horizontal.com>
2025-06-06 18:17:00 +0530

    refactor: clean up next.config.ts and ensure proper export of configuration

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,3 +1,5 @@
+export const runtime = 'nodejs';
+
 import { NextResponse } from 'next/server';
 import axios from 'axios';
 import SFTPClient from 'ssh2-sftp-client';
--- next.config.ts
+++ next.config.ts
@@ -3,22 +3,5 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-module.exports = {
-  resolve: {
-    extensions: ["...", ".node"],
-  },
-  target: "node",
-  node: {
-    __dirname: false,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.node$/,
-        loader: "node-loader",
-      },
-    ],
-  },
-};
 
 export default nextConfig;
